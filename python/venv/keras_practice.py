4079d1b28083831913df1a7911bb109964fe8402
witch_factory <soakdma37@gmail.com>
2020-07-15 21:40:33 +0900

    11

--- python/venv/keras_practice.py
+++ python/venv/keras_practice.py
@@ -1,85 +1,37 @@
 import tensorflow as tf
 from tensorflow import keras
+import pathlib
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-fashion_mnist=keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels)=fashion_mnist.load_data()
+dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
 
-class_names='T-shirts Trouser Pullover Dress Coat Sandal Shirt Sneaker Bag AnkleBoot'.split()
-train_images=train_images/255
-test_images=test_images/255
+column_names='MPG Cylinders Displacement Horsepower Weight Acceleration Model_Year Origin'.split()
+raw_dataset=pd.read_csv(dataset_path, names=column_names, na_values='?', comment='\t',
+                        sep=" ", skipinitialspace=True)
 
-"""plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid()
-    plt.imshow(train_images[i],cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()"""
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
-])
+dataset=raw_dataset.copy()
+dataset=dataset.dropna()
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy']
-              )
+origin=dataset.pop('Origin')
 
-model.fit(train_images, train_labels, epochs=5)
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-predictions=model.predict(test_images)
+train_dataset=dataset.sample(frac=0.8, random_state=0)
+test_dataset=dataset.drop(train_dataset.index)
 
-def plot_image(i, predictions_array, true_label, img):
-    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
+train_labels=train_dataset.pop('MPG')
+test_labels=test_dataset.pop('MPG')
 
-    plt.imshow(img, cmap=plt.cm.binary)
-    predicted_label=np.argmax(predictions_array)
-    if predicted_label==true_label:
-        color='blue'
-    else:color='red'
 
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                        100*np.max(predictions_array),
-                                        class_names[true_label]),
-               color=color)
+def norm(x): #normalization
+  return (x - train_stats['mean']) / train_stats['std']
 
-def plot_value_array(i, predictions_array, true_label):
-    predictions_array, true_label = predictions_array[i], true_label[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-    thisplot=plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0,1])
-    predicted_label=np.argmax(predictions_array)
 
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
 
-"""i=5675
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i,predictions, test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i,predictions, test_labels)
-plt.show()"""
 
-img=test_images[5675]
-print(img.shape)
-
-img=(np.expand_dims(img,0))
-
-predictions_single=model.predict(img)
-print(predictions_single)
-
-plot_value_array(5675,predictions, test_labels)
-_=plt.xticks(range(10), class_names, rotation=45)
-plt.show()
+def build_model():
+    
